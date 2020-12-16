20e32bebe5cb7e4932eebc853028bb4af42a527b
ceng ren <renceng@sina.com>
2020-12-16 15:44:16 +0800

    Create face.vue

---
+++ pages/face/face.vue
@@ -0,0 +1,32 @@
+<template>
+	<view class="face">
+		<vertify-mpsdk ref="verifyMpsdk"></vertify-mpsdk>
+	</view>
+</template>
+
+<script>
+	import vertifyMpsdk from '@/wxcomponents/verify_mpsdk/index/index';
+	export default {
+		components:{
+			vertifyMpsdk
+		},
+		data() {
+			return {
+
+			}
+		},
+		onLoad(i) {
+			// 页面onLoad的时候手动调用
+			this.$nextTick(() => {
+				this.$refs.verifyMpsdk.onLoad(i)
+			})
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
